6a6a595bf678716ff1164b0695b7273ea57adae3
alex.yatsko <alex.yatsko@kenmoredesign.com>
2024-02-23 00:58:09 +0300

    added

--- src/app/components/mainpage/mainpage.component.ts
+++ src/app/components/mainpage/mainpage.component.ts
@@ -16,24 +16,24 @@ import {
 })
 export class MainPageComponent implements OnInit, OnDestroy {
   imageObject: Array<any> = [{
-    image: './../../assets/images/1.png',
-    thumbImage: '../../../assets/images/1.png',
+    image: './../../lizacard/assets/images/1.png',
+    thumbImage: '../../../lizacard/assets/images/1.png',
     alt: 'alt of image',
   },
     {
-    image: './../../assets/images/2.png',
-    thumbImage: './../../assets/images/2.png',
+    image: './../../lizacard/assets/images/2.png',
+    thumbImage: './../../lizacard/assets/images/2.png',
     alt: 'alt of image',
   },
     {
-    image: './../../assets/images/3.png',
-    thumbImage: '../../../assets/images/3.png',
+    image: './../../lizacard/assets/images/3.png',
+    thumbImage: '../../../lizacard/assets/images/3.png',
     alt: 'Image alt',
     order: 1 //Optional: if you pass this key then slider images will be arrange according @input: slideOrderType
   },
     {
-      image: './../../assets/images/4.png',
-      thumbImage: '../../../assets/images/4.png',
+      image: './../../lizacard/assets/images/4.png',
+      thumbImage: '../../../lizacard/assets/images/4.png',
       alt: 'alt of image',
     },
     {
